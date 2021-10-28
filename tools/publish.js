089cd3f742957c5371b853d7a2ef2b5900e8680f
liuyuan <373005226@qq.com>
2021-10-28 11:00:04 +0800

    添加publish发布脚本

---
+++ tools/publish.js
@@ -0,0 +1,25 @@
+// const spawn = require('cross-spawn')
+// const inquirer = require('inquirer')
+import spawn from 'cross-spawn'
+import inquirer from 'inquirer'
+
+inquirer.prompt([
+    {
+      type: 'list',
+      name: 'version',
+      message: '选择发布版本',
+      choices: [
+        'patch',
+        'minor',
+        'major',
+      ],
+      default: 'patch'
+    }
+  ]).then((answers) => {
+      spawn.sync('npm', ['version', answers.version], { stdio: 'inherit' })
+      spawn.sync('npm', ['publish'], { stdio: 'inherit' })
+    // spawn.sync('npm', ['run', 'log'], { stdio: 'inherit' });
+    // spawn.sync('git', ['add', 'CHANGELOG.md'], { stdio: 'inherit' });
+    // spawn.sync('git', ['commit', '-m', 'changelog'], { stdio: 'inherit' });
+    // spawn.sync('git', ['push'], { stdio: 'inherit' });
+  });
